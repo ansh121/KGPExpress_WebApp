9c005f8e6e91efa62b687631029bc562833e8949
Nilesh015 <nileshmandal015@gmail.com>
2021-03-08 13:03:25 +0530

    added calendar and event pages

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,16 @@
+from django.conf.urls import url
 from django.urls import path
+
+from . import views
 from .views import login_view, register_user, home
 from django.contrib.auth.views import LogoutView
 
+app_name = 'authentication'
 urlpatterns = [
-    path('', home, name="home"),
+    #path('', home, name="home"),
+    path('', views.HomeView.as_view(), name='calendar'),
+    url(r'^event/new/$', views.event, name='event_new'),
+    url(r'^event/edit/(?P<event_id>\d+)/$', views.event, name='event_edit'),
     path('login/', login_view, name="login"),
     path('register/', register_user, name="register"),
     path("logout/", LogoutView.as_view(), name="logout"),
--- authentication/views.py
+++ authentication/views.py
@@ -7,8 +7,22 @@ from .forms import LoginForm, SignUpForm, SearchForm
 from verify_email.email_handler import send_verification_email
 from validate_email import validate_email
 
+from datetime import datetime, timedelta, date
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+import calendar
+
+from .models import *
+from .utils import Calendar
+from .forms import EventForm
+
 import environ
+
 env = environ.Env()
+
 
 def login_view(request):
     form = LoginForm(request.POST or None)
@@ -25,7 +39,7 @@ def login_view(request):
             if user is not None:
                 login(request, user)
                 return redirect("/")
-                
+
             else:
                 msg = 'Authentication Failed'
         else:
@@ -42,13 +56,13 @@ def register_user(request):
         form = SignUpForm(request.POST)
         try:
             invalid_user = User.objects.get(username=request.POST['username'])
-            if invalid_user.is_active==False:
+            if invalid_user.is_active == False:
                 invalid_user.delete()
         except:
             print('new user')
         finally:
             if form.is_valid():
-                
+
                 # form.save()
                 # username = form.cleaned_data.get("username")
                 # raw_password = form.cleaned_data.get("password1")
@@ -59,9 +73,9 @@ def register_user(request):
 
                 # is_valid = validate_email(email_address=form.cleaned_data.get("email"), check_mx=True)
                 # is_valid = validate_email(form.cleaned_data.get("email"), verify=True)
-                is_valid=True
-                if(is_valid==False):
-                    msg="Email does not exist!"
+                is_valid = True
+                if (is_valid == False):
+                    msg = "Email does not exist!"
                 else:
                     inactive_user = send_verification_email(request, form)
                     # print(inactive_user)
@@ -77,7 +91,7 @@ def register_user(request):
                 # print(form.errors.items())
                 for field, errors in form.errors.items():
                     for error in errors:
-                        msg=error
+                        msg = error
                         break
 
                 # msg = 'Form is not valid'
@@ -88,6 +102,7 @@ def register_user(request):
 
     return render(request, "accounts/register.html", {"form": form, "msg": msg, "success": success})
 
+
 def home(request):
     # if request.method == "POST":
     form = SearchForm(request.POST or None)
@@ -95,3 +110,60 @@ def home(request):
         print(form.cleaned_data.items())
         print(request.POST)
     return render(request, "index.html", {"form": form})
+
+
+class HomeView(generic.ListView):
+    model = Event
+    template_name = 'index.html'
+
+    # template_name = 'authentication/modified_calendar.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        d = get_date(self.request.GET.get('month', None))
+        cal = Calendar(d.year, d.month)
+        form = SearchForm(self.request.POST or None)
+        if form.is_valid():
+            print(form.cleaned_data.items())
+            print(self.request.POST)
+        html_cal = cal.formatmonth(withyear=True)
+        context['calendar'] = mark_safe(html_cal)
+        context['prev_month'] = prev_month(d)
+        context['next_month'] = next_month(d)
+        context['form'] = form
+        return context
+
+def get_date(req_month):
+    if req_month:
+        year, month = (int(x) for x in req_month.split('-'))
+        return date(year, month, day=1)
+    return datetime.today()
+
+
+def prev_month(d):
+    first = d.replace(day=1)
+    prev_month = first - timedelta(days=1)
+    month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
+    return month
+
+
+def next_month(d):
+    days_in_month = calendar.monthrange(d.year, d.month)[1]
+    last = d.replace(day=days_in_month)
+    next_month = last + timedelta(days=1)
+    month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
+    return month
+
+
+def event(request, event_id=None):
+    instance = Event()
+    if event_id:
+        instance = get_object_or_404(Event, pk=event_id)
+    else:
+        instance = Event()
+
+    form = EventForm(request.POST or None, instance=instance)
+    if request.POST and form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse('authentication:calendar'))
+    return render(request, 'event.html', {'form': form})
