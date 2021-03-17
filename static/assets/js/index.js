75ab6394ef400a85087362efd993c60e3882584b
Anshul Choudhary <achoudhary718@gmail.com>
2021-03-17 17:03:35 +0530

    completed search box functionality

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -1,1 +1,12 @@
-$('.basicAutoSelect').autoComplete();
+$.getJSON("/static/assets/data/subject-list.json",
+    function (data, textStatus, jqXHR) {
+        var options = "";
+        $.each(data, function (key, val) { 
+            options += '<option value="' + val['value'] +' - '+ val['text'] + '" />';
+        });
+
+        // $("#subject-list").innerHTML = options;
+        document.getElementById('subject-list').innerHTML = options;
+        console.log(options);
+    }
+);
