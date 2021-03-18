bbe7982d6d914931177aff1e33a36ac493baa384
Anshul Choudhary <achoudhary718@gmail.com>
2021-03-18 15:04:34 +0530

    .env configured for env var setting

--- core/settings.py
+++ core/settings.py
@@ -21,9 +21,10 @@ env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-
-# reading .env file
-# environ.Env.read_env(env_file='.env')
+# checking if .env exists file
+if os.path.exists('.env') and os.path.isfile('.env'):
+    # reading .env file
+    environ.Env.read_env(env_file='.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
