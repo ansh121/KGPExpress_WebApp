02425f60e7daaeaddcea27afe62484be8c9838a1
ayush774 <ayush2017@iitkgp.ac.in>
2021-04-01 14:02:33 +0530

    Added Password Reset

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LogoutView
-
+from django.contrib.auth.views import *
+from .forms import *
 app_name='authentication'
 urlpatterns = [
     path('login/', views.login_view, name="login"),
     path('register/', views.register_user, name="register"),
     path("logout/", LogoutView.as_view(template_name='accounts/logout.html'), name="logout"),
+    path("password_reset/", PasswordResetView.as_view(template_name='accounts/password_reset.html', subject_template_name='accounts/subject.txt', html_email_template_name='accounts/password_reset_confirm_email.html'), name="password_reset"),
+    path("password_reset/done/", PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name="password_reset_done"),
+    path("reset/<uidb64>/<token>/", PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html',form_class=ForgetPasswordForm), name='password_reset_confirm'),
+    path("reset/done/", PasswordResetCompleteView.as_view(template_name='accounts/reset_done.html'), name="reset_done"),
 ]
