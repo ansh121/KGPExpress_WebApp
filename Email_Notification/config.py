581594d3fd4c75fec33a9d2a12be45315ac05496
Ayush Kumar <78661202+ayush774@users.noreply.github.com>
2021-04-03 21:03:45 +0530

    Update config.py

--- Email_Notification/config.py
+++ Email_Notification/config.py
@@ -1,8 +1,3 @@
-# This file was adapted from http://www.postgresqltutorial.com/postgresql-python/connect/
-
-# The following config() function reads in the database.ini file and returns the connection
-# parameters as a dictionary. This function will be imported in to the main python script:
-
 #!/usr/bin/python
 from configparser import ConfigParser
  
