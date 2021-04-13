d78b71f5d2328dcdb499355e856e36aa3ca0bfc0
Anshul Choudhary <achoudhary718@gmail.com>
2021-04-14 00:15:19 +0530

    notification checkbox correction

--- static/assets/js/profile.js
+++ static/assets/js/profile.js
@@ -1,22 +1,32 @@
-$(function () {
+console.log($('#notification_checkbox').is(":checked"))
+if ($('#notification_checkbox').is(":checked")) 
+    $('#notification_checkbox').prop('checked', true);
+else 
+    $('#notification_checkbox').prop('checked', false);
+// console.log(this.checked)
 
-    $('#notification').change(function() {
-        if(this.checked) {
-            $.ajax({
-                type: "POST",
-                url: "notification/",
-                data: { notification: false },
-              });
-              $('#notification').prop('checked', false); 
-        }
-        else{
-            $.ajax({
-                type: "POST",
-                url: "notification/",
-                data: { notification: true },
-              });
-              $('#notification').prop('checked', true);
-        }
-    });
-   
+// $(function () {
+
+$('#notification_checkbox').change(function() {
+    // console.log(this.checked)
+    if(this.checked) {
+        $.ajax({
+            type: "POST",
+            url: "notification/",
+            data: { notification: true },
+            });
+            // $('#notification').prop('checked', false); 
+            $('#notification_checkbox').html('checked');
+    }
+    else{
+        $.ajax({
+            type: "POST",
+            url: "notification/",
+            data: { notification: false },
+            });
+            // $('#notification').prop('checked', true);
+            $('#notification_checkbox').html('unchecked');
+    }
 });
+   
+// });
